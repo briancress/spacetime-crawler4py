d8a1065ffbd27c49b0d1a7642a5cba89714870d0
Roy Richa <rricha@uci.edu>
2024-04-15 19:05:35 -0700

    Extract Next Link

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import re
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -15,7 +16,25 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+
+    # Filter for an unsuccessful status code
+    if resp.status != 200:
+        return []
+
+    # Parse the HTML content
+    soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+
+    # Extract links from the page
+    links = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href:
+            full_url = urljoin(resp.url, href)
+            if is_valid(full_url):
+                links.append(full_url)
+
+    return links
+    # return list()
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
