f798af3e0910c99e519d16ad4532d4e5df815145
Roy Richa <rricha@uci.edu>
2024-04-18 22:24:24 +0000

    Added variables that we need to keep track of. Also going to add responses for different error server codes.

--- scraper.py
+++ scraper.py
@@ -2,8 +2,31 @@ import re
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 
+unique_pages = set()
+longest_page_words = 0
+word_frequency = {}
+subdomains = {}
+
 def scraper(url, resp):
+    # global unique_pages
+    # global longest_page_words
+    # global word_frequency
+    # global subdomains
+
     links = extract_next_links(url, resp)
+
+    unique_pages.update(links)
+
+    # Update longest page word count
+    for link in links:
+        words = link.split('/')
+        word_count = len(words[-1].split('-')) if words[-1] else 0
+        longest_page_word_count = max(longest_page_word_count, word_count)
+
+    update_word_frequency(resp.raw_response.content)
+
+    update_subdomain()
+
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
@@ -57,3 +80,36 @@ def is_valid(url):
     except TypeError:
         print ("TypeError for ", parsed)
         raise
+
+update_word_frequency(response_content):
+    # global word_frequency
+
+    english_stopwords = {
+    "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", 
+    "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", 
+    "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", 
+    "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", 
+    "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", 
+    "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", 
+    "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", 
+    "other", "ought", "our", "ours"
+    }
+
+    extracted_words = re.findall(r'\b[A-Za-z]+\b', content.decode('utf-8'))
+
+    for word in words:
+        word = word.lower()
+
+        if word not in word_frequency and word not in english_stopwords:
+            word_frequency[word] = 0
+        word_frequency[word] += 1
+
+
+update_subdomain(url):
+    # global subdomains
+
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc.split('.')[0]
+    if domain not in subdomains:
+        subdomains[domain] = 0
+    subdomains[domain] += 1
