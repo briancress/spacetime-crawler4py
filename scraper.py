4f45f3d7977e0f2e0254ee2e9095edb8ee14de92
Brian Cress <bcress@uci.edu>
2024-04-20 18:48:47 +0000

    Added and changed functions regarding the report questions, possibly done, still need to fix trap errors, currently in an infinite loop crawling an incrementing page

--- scraper.py
+++ scraper.py
@@ -2,31 +2,57 @@ import re
 from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup
 
+# Set of unique pages
 unique_pages = set()
-longest_page_words = 0
+# List containing url at index 0, count at index 1
+longest_page_words = ['page_url', 0]
+# Dictionary containing [word] = # of occurrences
 word_frequency = {}
+# Dictionary containing [subdomain] = # of occurrences
 subdomains = {}
+# Set of all visited urls
+visited_urls = set()
 
 def scraper(url, resp):
     global unique_pages
     global longest_page_words
     global word_frequency
     global subdomains
+    global visited_urls
 
     links = extract_next_links(url, resp)
 
-    unique_pages.update(links)
+    # Return empty links and don't count if bad status
+    if links == []:
+        return []
+
+    # Add url to visited
+    # visited_urls contains all visited urls, the entire url (not used for counting unique urls, only for not re-visiting)
+    visited_urls.add(url)
+
+    # Uniqueness is only established by URL, not fragment
+    # Get rid of fragment after '#' and add to unique pages
+    no_fragment_url = url.split("#")[0]
+    unique_pages.add(no_fragment_url)
 
     # Update longest page word count
-    for link in links:
-        words = link.split('/')
-        word_count = len(words[-1].split('-')) if words[-1] else 0
-        longest_page_words = max(longest_page_words, word_count)
+    update_longest_word_page(url, resp.raw_response.content)
 
+    # I think this is counting the words in the link instead of the words in the page
+    # for link in links:
+    #     words = link.split('/')
+    #     word_count = len(words[-1].split('-')) if words[-1] else 0
+    #     longest_page_words = max(longest_page_words, word_count)
+
+    # Update word frequency for current URL page
     update_word_frequency(resp.raw_response.content)
 
+    # Total subdomains in the ics.uci.edu domain
     update_subdomain(url)
 
+    print(f"Visiting url : '{url}'")
+
+    #return [link for link in links if is_valid(link)]
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
@@ -44,25 +70,28 @@ def extract_next_links(url, resp):
     if resp.status != 200:
         return []
 
-    # Parse the HTML content
+    # Parse HTML with beautiful soup
     soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
 
-    # Extract links from the page
+    # Extract links from the html
     links = []
+    # Find all links in the html
     for link in soup.find_all('a'):
+        # Get href from each link
         href = link.get('href')
+        # Join the original url with the new href and append
         if href:
             full_url = urljoin(resp.url, href)
-            #if is_valid(full_url):
             links.append(full_url)
 
     return links
-    # return list()
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
+    global visited_urls
+
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
@@ -70,6 +99,8 @@ def is_valid(url):
         if not re.match(r"(www\.)?(ics|cs|informatics|stat)\.uci\.edu", parsed.netloc):
             return False
         if not re.match(r"/\w+", parsed.path):
+            return False
+        if url in visited_urls:
             return False
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
@@ -85,7 +116,24 @@ def is_valid(url):
         print ("TypeError for ", parsed)
         raise
 
-def update_word_frequency(response_content):
+def update_longest_word_page(url, page_content):
+    global longest_page_words
+
+    # Parse the page content with beautiful soup
+    soup = BeautifulSoup(page_content, 'html.parser')
+    # Get the actual text content fromt he created soup
+    text_content = soup.get_text(separator = ' ')
+    # extract all the words using regex
+    extracted_words = re.findall(r'\b[A-Za-z]+\b', text_content)
+
+    word_count = len(extracted_words)
+
+    if (word_count > longest_page_words[1]):
+        longest_page_words[0] = url
+        longest_page_words[1] = word_count
+
+
+def update_word_frequency(page_content):
     global word_frequency
 
     english_stopwords = {
@@ -99,8 +147,14 @@ def update_word_frequency(response_content):
     "other", "ought", "our", "ours"
     }
 
-    extracted_words = re.findall(r'\b[A-Za-z]+\b', response_content.decode('utf-8'))
+    # Parse the page content with beautiful soup
+    soup = BeautifulSoup(page_content, 'html.parser')
+    # Get the actual text content fromt he created soup
+    text_content = soup.get_text(separator = ' ')
+    # extract all the words using regex
+    extracted_words = re.findall(r'\b[A-Za-z]+\b', text_content)
 
+    # Update frequency for each word
     for word in extracted_words:
         word = word.lower()
 
@@ -115,8 +169,18 @@ def update_word_frequency(response_content):
 def update_subdomain(url):
     global subdomains
 
+    # Only count subdomains in the ics.uci.edu domain
     parsed_url = urlparse(url)
-    domain = parsed_url.netloc.split('.')[0]
-    if domain not in subdomains:
-        subdomains[domain] = 0
-    subdomains[domain] += 1
+    # Get subdomain by splitting once at the first period
+    # subdomain = [vision].ics.uci.edu
+    subdomain = parsed_url.netloc.split('.', 1)[0]
+    #Get domain
+    # domain = [vision.ics.uci.edu]
+    domain = parsed_url.netloc
+
+    # Check if the domain ends with ics.uci.edu
+    if domain.endswith('ics.uci.edu'):
+        # Increment the subdomain's count
+        if subdomain not in subdomains:
+            subdomains[subdomain] = 0
+        subdomains[subdomain] 
